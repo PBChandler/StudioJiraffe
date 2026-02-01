91daa224756a0dfb6a84b611a3536f29c633768a
westwardo5 <adam.smulovics@outlook.com>
2026-01-31 22:20:26 -0500

    Bomb

--- StudioJiraffe/Assets/HookInteractable.cs
+++ StudioJiraffe/Assets/HookInteractable.cs
@@ -1,16 +1,37 @@
+using System;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class HookInteractable : MonoBehaviour
 {
+    private Rigidbody2D myRigidbody;
+    public Rigidbody2D releaseWindow;
+    private Collider2D myCollider; 
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "PlayerHook")
         {
-            Event();
+            
+            EnterEvent(collision, myRigidbody, myCollider);
+        }
+
+        
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Body")
+        {
+            ExitEvent(collision, myRigidbody);
         }
     }
 
-    public virtual void Event()
+    public virtual void EnterEvent(Collider2D collision, Rigidbody2D myRigidbody, Collider2D mycollider)
+    {
+        
+    }
+
+    public virtual void ExitEvent(Collider2D collision, Rigidbody2D myRigidbody)
     {
 
     }
--- StudioJiraffe/Assets/bomb.cs
+++ StudioJiraffe/Assets/bomb.cs
@@ -1,10 +1,49 @@
+using System.Collections;
+using TreeEditor;
 using UnityEngine;
 
 public class bomb : HookInteractable
 {
-    public override void Event()
+    public float distance;
+    [SerializeField]
+    ParticleSystem myParticles;
+  
+    public override void EnterEvent(Collider2D other, Rigidbody2D myRigidbody, Collider2D myCollider)
     {
+        //if (other.gameObject.tag == "PlayerHook")
+        //{
+        //myRigidbody.bodyType = RigidbodyType2D.Kinematic ;
+
+        //Transform child = other.gameObject.transform.GetChild(1);
+        transform.position = other.transform.position;
+        transform.SetParent(other.transform);
+        transform.localPosition = new Vector2(0f, distance);
+
+        //}
+        //myCollider.isTrigger = true; 
+
+        base.EnterEvent(other, myRigidbody, myCollider);
+
         
-        base.Event();
+
+    }
+
+    public override void ExitEvent(Collider2D other, Rigidbody2D myRigidbody)
+    {
+        StartCoroutine(explode(myRigidbody));
+        
+
+        base.ExitEvent(other, myRigidbody);
+
+    }
+    
+    IEnumerator explode (Rigidbody2D myRigidbody)
+    {
+
+        yield return new WaitForSeconds(1f);
+        transform.parent = null;
+        myRigidbody.bodyType = RigidbodyType2D.Dynamic;
+        myParticles.enableEmission = true;
+        Destroy(gameObject); 
     }
 }
